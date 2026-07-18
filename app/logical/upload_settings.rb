67d5b125cf50c2be80879649de6dcd35b9416597
Donovan_DMC <hewwo@yiff.rocks>
2026-07-18 02:59:01 -0500

    Fix upload settings

--- app/logical/upload_settings.rb
+++ app/logical/upload_settings.rb
@@ -3,7 +3,7 @@
 class UploadSettings
   include(ActiveModel::Serializers::JSON)
 
-  ATTRIBUTES = %i[compact_mode safe_site post_tags upload_tags recent_tags allow_locked_tags allow_rating_lock allow_upload_as_pending allow_uploading_in_progress max_file_size max_file_size_map max_file_size_per_request].freeze
+  ATTRIBUTES = %i[compact_mode safe_site post_tags upload_tags recent_tags allow_locked_tags allow_rating_lock allow_upload_as_pending allow_uploading_as_in_progress max_file_size max_file_size_map max_file_size_per_request].freeze
 
   attr_accessor(:user, :post)
 

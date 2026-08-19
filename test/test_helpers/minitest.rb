d168c099d43b30912adda82d102e7b4961770131
Donovan_DMC <hewwo@yiff.rocks>
2026-08-19 05:50:01 -0500

    Fix stale minitest helper

--- test/test_helpers/minitest.rb
+++ test/test_helpers/minitest.rb
@@ -6,10 +6,10 @@
 module Minitest
   module Assertions
     def assert_equal(exp, act, msg = nil)
-      assert(exp == act, message(msg, E) { diff(exp, act) }) # rubocop:disable Minitest/AssertOperator, Minitest/AssertEqual, Minitest/AssertWithExpectedArgument
+      assert(exp == act, message(msg, nil) { diff(exp, act) }) # rubocop:disable Minitest/AssertOperator, Minitest/AssertEqual, Minitest/AssertWithExpectedArgument
     end
   end
 end
 
 # line number should be where the assert above is
-Rails.backtrace_cleaner.add_silencer { |line| line =~ %r{test/test_helpers/minitest\.rb:7} }
+Rails.backtrace_cleaner.add_silencer { |line| line =~ %r{test/test_helpers/minitest\.rb:9} }
